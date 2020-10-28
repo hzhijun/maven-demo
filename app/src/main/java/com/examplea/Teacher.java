f916ec31cbd2c45f24c6bf27cc11a5f51b2c264b
zhijun.han <zhijun.han@rayjoy.com>
2020-10-28 10:29:38 +0800

    first commit

---
+++ app/src/main/java/com/examplea/Teacher.java
@@ -0,0 +1,10 @@
+package com.examplea;
+
+public class Teacher {
+    public String GetTeacher(){
+        return "examplea";
+    }
+    public String GetTeachers(){
+        return "examplea";
+    }
+}
